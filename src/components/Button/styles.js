d5bb32e908c2d206109fa4e6d74fd89e564fc127
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2023-03-09 21:39:48 -0300

    Calculadora em React

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 5px solid #FFFFFF;
+    background-color: #DBA40E;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #DC8449;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #DC8449;
+        border: 3px solid #FFFFFF;
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 44px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
